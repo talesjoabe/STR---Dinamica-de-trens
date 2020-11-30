730a367b55e8eaae0351e77aad0906dcc5ce5200
talesjoabe <30672549+talesjoabe@users.noreply.github.com>
2020-11-30 03:34:01 -0300

    Refatoração : função inutilizada

--- functions.py
+++ functions.py
@@ -1,31 +1,6 @@
 import arcade
 import os
 
-train_width =  20
-train_height = 10
-velocity = 1
-
-xc_t1 = 75 
-yc_t1 = 482
-colour_t1 = arcade.color.ELECTRIC_GREEN
- 
-xc_t2 = 185 
-yc_t2 = 482
-colour_t2 = arcade.color.PURPLE
-
-xc_t3 = 295
-yc_t3 = 482
-colour_t3 = arcade.color.RED
-
-xc_t4 = 75
-yc_t4 = 412 
-colour_t4 = arcade.color.BLUE
-
-def move_train(xc,yc, width_rail, height_rail, velocity):
-	count = 1
-	while(count < width_rail):
-		xc = xc+velocity
-		count=count+1
 def drawRails():
 	# TRILHO-RETANGULO
 	arcade.draw_rectangle_outline(115, 450, 105, 65,arcade.color.ELECTRIC_GREEN)
