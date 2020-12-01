65a8486e05cdfda2b4eb5b0e363237ea6fad4863
talesjoabe <30672549+talesjoabe@users.noreply.github.com>
2020-12-01 06:42:14 -0300

    refactor

--- train.py
+++ train.py
@@ -3,6 +3,10 @@ import time # tempo (opcional)
 import arcade
 import os
 
+TREM1 = arcade.color.ELECTRIC_GREEN
+TREM2 = arcade.color.PURPLE
+TREM3 = arcade.color.RED
+TREM4 = arcade.color.BLUE
 
 # define uma classe para a criacao de threads
 class train:
@@ -21,40 +25,29 @@ class train:
 		self.count_2 = 1
 		self.count_3 = 1
 		self.count_4 = 1
+		self.velocity_aux = velocity
+		self.state="L0"
 	
 	def drawTrain(self):
 		arcade.draw_rectangle_filled(self.xc, self.yc, self.width_train, self.height_train , self.colour)
 	
-	def moveTrain(self):
-		if self.count_1 < self.width_rail-self.width_train:		
-			self.xc = self.xc+self.velocity
-			self.count_1=self.count_1+1
+	def updateState(self, state):
+		self.state = state
+	
+	def moveRight(self):
+		self.xc = self.xc+self.velocity
+		self.height_train = 10
+		self.width_train = 20
 
-		elif self.count_2 < self.height_rail:
-			self.width_train = 10
-			self.height_train = 20
-			self.yc = self.yc-self.velocity
-			self.count_2= self.count_2+1
-			self.count_3 = self.count_1
-		elif self.count_3 > -10:
-			self.width_train = 20
-			self.height_train = 10
-			self.xc = self.xc-self.velocity
-			self.count_3= self.count_3-1
-			self.count_4 = self.count_2
-		elif self.count_4 > -1 :
-			self.width_train = 10
-			self.height_train = 20
-			self.yc = self.yc+self.velocity
-			self.count_4= self.count_4-1
-		else:
-			self.count_1 = 1
-			self.count_2 = 1
-			self.width_train = 20
-			self.height_train = 10
-			self.xc = self.xc_aux
-			self.yc = self.yc_aux
-			
-			
-		
-				
+	def moveDown(self):
+		self.yc = self.yc-self.velocity
+		self.height_train = 20
+		self.width_train = 10
+	def moveLeft(self):
+		self.xc = self.xc-self.velocity
+		self.height_train = 10
+		self.width_train = 20
+	def moveUp(self):
+		self.yc = self.yc+self.velocity
+		self.height_train = 20
+		self.width_train = 10
